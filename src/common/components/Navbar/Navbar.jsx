6c1c91dd8d929d32fc7e187efd1d6adfd5f949fd
Vladislav Starikov <vlad.starikov.88@gmail.com>
2019-05-04 23:40:24 +0300

    styles: added styles

--- src/common/components/Navbar/Navbar.jsx
+++ src/common/components/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Navbar = ({stations}) => (
-  <div>
-    <ul>
-      { stations.map(station => (
-          <li key={station.id}>
-            <h3>{station.name}</h3>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-)
+import style from './Navbar.module.scss';
 
+class Navbar extends Component {
+  render() {
+    const {stations} = this.props;
+    return (
+      <div className={style.navbar}>
+        <ul className={style.navbar__group}>
+          { stations.map(station => (
+              <li 
+                key={station.id}
+                className={style.navbar__item}
+              >
+                <a href="/">{station.name}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )    
+  }
+}
 export default Navbar;
